a42a4fb3d6a27305241c58ce11fb6eeb606c1f8f
Ariba2023 <148465284+Ariba2023@users.noreply.github.com>
2024-09-19 01:11:53 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Footer from "./Components/Footer/Footer";
 import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Navbar />
       <Hero/>
       <About/>
+      <Services/>
+      <MyWork/>
+      <Contact/>
       <Footer/>
     </div>
   );
