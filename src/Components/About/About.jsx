ca8659c0d21a8b91c7d283ba7f5dde5dd57dc03d
Ariba2023 <148465284+Ariba2023@users.noreply.github.com>
2024-09-19 00:20:20 +0500

    commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
 import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/Opentowork.jpg'
 
 const About = () => {
   return (
@@ -8,7 +9,21 @@ const About = () => {
       <div className="about-title">
 
         <h1>About Me</h1>
-        <img src="" alt="" />
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>I am a Web developer</p>
+            <p>Ariba Junejo</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}} /> </div>
+          </div>
+        </div>
       </div>
     </div>
   );
