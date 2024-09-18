a82ec2737795f1b1147fa6a50bea950d8a864898
Ariba2023 <148465284+Ariba2023@users.noreply.github.com>
2024-09-19 00:54:50 +0500

    About section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./About.css";
-import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/Opentowork.jpg'
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/Opentowork.jpg";
 
 const About = () => {
   return (
     <div className="about">
       <div className="about-title">
-
         <h1>About Me</h1>
         <img src={theme_pattern} alt="" />
       </div>
@@ -17,12 +16,43 @@ const About = () => {
         </div>
         <div className="about-right">
           <div className="about-para">
-            <p>I am a Web developer</p>
-            <p>Ariba Junejo</p>
+            <p>I am a Web developer jahsjkhajdhkasjhdjkashdjkahdjkshjkashdwdjkahdjhsdjhashasjkfhajfhdfjhfgsdfhgsdhfgsdhajashahdahj</p>
+            <p>Ariba Junejo asjdjashdjkdhakjdhkajhdkjahdjkghadnajksfdhfghsdfgsjhgfjdhgfhgdhdjkashdjkash</p>
           </div>
           <div className="about-skills">
-            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}} /> </div>
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />{" "}
+            </div>
+            <div className="about-skill">
+              <p>React Js</p>
+              <hr style={{ width: "60%" }} />{" "}
+            </div>
+            <div className="about-skill">
+              <p>Laravel</p>
+              <hr style={{ width: "70%" }} />{" "}
+            </div>
+            <div className="about-skill">
+              <p>SQL</p>
+              <hr style={{ width: "50%" }} />{" "}
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>2+</h1>
+          <p>Years of experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>20+</h1>
+          <p>Project Completed</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>Happy clients</p>
         </div>
       </div>
     </div>
