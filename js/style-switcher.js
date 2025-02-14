169d3dbe327bf51322438b27b0a467e9ed7013d3
Ariba2023 <148465284+Ariba2023@users.noreply.github.com>
2025-02-15 02:08:07 +0500

    commit

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -39,15 +39,15 @@ dayNight.addEventListener("click", () => {
 
 })
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
 
 
-if ( document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun");
-} else {
-    dayNight.querySelector("i").classList.add("fa-moon");
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
 
 
 
-}
+    }
 })
